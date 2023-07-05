d72f733b2cd11e2e0ef24a58fa5d10b8b1d87d5c
CrayonPig <475003925@qq.com>
2023-07-05 21:44:11 +0800

    feat: 增加算法

--- docs/.vuepress/algorithm-route.js
+++ docs/.vuepress/algorithm-route.js
@@ -1,5 +1,16 @@
 module.exports = {
   '/algorithm/': [
     '/algorithm/',
+    '/algorithm/2667',
+    '/algorithm/2620',
+    '/algorithm/2704',
+    '/algorithm/2665',
+    '/algorithm/2635',
+    '/algorithm/2634',
+    '/algorithm/2629',
+    '/algorithm/2626',
+    '/algorithm/2703',
+    '/algorithm/2666',
+    '/algorithm/2623',
   ]
 }
